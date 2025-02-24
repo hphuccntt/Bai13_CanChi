20b4ff7118144a556430d046d0eb1491540ed1f1
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2025-02-24 13:42:22 +0700

    thêm phần for

--- Form1.cs
+++ Form1.cs
@@ -23,6 +23,12 @@ namespace Bai13_CanChi
             string[] chiArr = { "Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mẹo", "Thìn", "Tỵ", "Ngọ", "Mùi" };
             string giatritrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + " " + chiArr[Convert.ToInt32(txtNam.Text) % 12];
             txtKQ.Text = giatritrave;
+
+            for (int nam = 2000; nam <= 2050; nam++)
+            {
+                lsbNam.Items.Add(nam + " - " + canArr[nam % 10] + " " + chiArr[nam % 12]);
+
+            }
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -32,11 +38,7 @@ namespace Bai13_CanChi
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int nam = 2000; nam <= 2050; nam++)
-            {
-                lsbNam.Items.Add(nam);
-                
-            }
+            
         }
     }
 }
